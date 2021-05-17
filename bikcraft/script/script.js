569880f807ceec389925d656784eca3b8c7681a0
Caroline Steffen Vieira <carolsteffen98@gmail.com>
2021-05-16 21:24:51 -0300

    I'm learning about SEO

--- bikcraft/script/script.js
+++ bikcraft/script/script.js
@@ -16,3 +16,12 @@ new SimpleSlide({
 if(window.SimpleAnime){
 new SimpleAnime();
 }
+
+if(window.SimpleForm){
+new SimpleForm({
+  form: ".formphp",
+  button: "#enviar",
+  erro: "<div id='form-erro'><p><strong>Um erro ocorreu</strong>, tente enviar para o email carolsteffen98@gmail.</p></div>",
+  sucesso: "<div id='form-sucesso'><h2>Seu formulário foi enviado com sucesso!</h2><p>Em breve eu entro em contato com você.</p></div>"
+});
+}
